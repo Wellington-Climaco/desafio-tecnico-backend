919da8e6b4794ffd910c9f4489b08dee3714c3ab
Wellington <wellington.climaco9@gmail.com>
2025-03-26 23:24:25 -0300

    feat: add validation para request

--- src/main/java/com/example/desafio_tecnico_backend/request/CriaAgendamentoRequest.java
+++ src/main/java/com/example/desafio_tecnico_backend/request/CriaAgendamentoRequest.java
@@ -2,6 +2,10 @@ package com.example.desafio_tecnico_backend.request;
 
 import com.example.desafio_tecnico_backend.entities.Agendamento;
 import com.example.desafio_tecnico_backend.enums.TipoEnvio;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import java.util.Date;
@@ -9,9 +13,17 @@ import java.util.Date;
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class CriaAgendamentoRequest {
+
+    @NotNull(message = "DataHora não pode ser nulo.")
     public Date DataHora;
+
+    @NotBlank(message = "Destinatario não pode ser vazio.")
     public String Destinatario;
+
+    @NotBlank (message = "Mensagem não pode ser vazia.")
     public String Mensagem;
+
+    @NotBlank (message = "Envio não pode ser vazio.")
     public String Envio;
 
     public Agendamento MapToEntity(){
